aaf27fbe8920342d3d89b323dcf02d4136596cb9
Alexey <kesler@mail.ru>
2016-05-11 08:16:02 +0500

    Fix by dates service

--- src/main/java/org/kesler/mfc/routeforms/server/repository/RouteFormRepository.java
+++ src/main/java/org/kesler/mfc/routeforms/server/repository/RouteFormRepository.java
@@ -15,11 +15,13 @@ import java.util.UUID;
 public interface RouteFormRepository {
     List<RouteForm> findRouteForms() throws DataAccessException;
     List<RouteForm> findRouteFormsByDates(LocalDate begDate, LocalDate endDate) throws DataAccessException;
+    List<RouteForm> findRouteFormsByBegDate(LocalDate begDate) throws DataAccessException;
     RouteForm findRouteFormById(UUID id) throws DataAccessException;
     List<RouteForm> findRouteFormsByAuto(Auto auto) throws DataAccessException;
     List<RouteForm> findRouteFormsByAutoAndDates(Auto auto, LocalDate begDate, LocalDate endDate) throws DataAccessException;
     List<RouteForm> findRouteFormsByBranch(Branch branch) throws DataAccessException;
     List<RouteForm> findRouteFormsByBranchAndDates(Branch branch, LocalDate begDate, LocalDate endDate) throws DataAccessException;
+    List<RouteForm> findRouteFormsByBranchAndBegDate(Branch branch, LocalDate begDate) throws DataAccessException;
     void save(RouteForm routeForm) throws DataAccessException;
     void remove(UUID id) throws DataAccessException;
 }
