82842cca257c83a9582b184da2ab965c54dcfd33
Alex K <kesler.dev@gmail.com>
2022-01-21 01:17:15 +0300

    Переключил на последние библиотеки + Spring Boot

---
+++ src/main/java/org/kesler/mfc/routeforms/server/RouteFormsServer.java
@@ -0,0 +1,13 @@
+package org.kesler.mfc.routeforms.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteFormsServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteFormsServer.class, args);
+    }
+
+}
